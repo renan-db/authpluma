50397503ecbab5db41f1e617ff831339b7c5f192
renan-db <renan.tsx@gmail.com>
2025-02-26 10:19:37 -0300

    feat: configura conexão com PostgreSQL
    - Adiciona configuração do PostgreSQL no docker-compose
    - Implementa conexão com banco de dados
    - Configura healthcheck para garantir inicialização correta do banco
    - Atualiza variáveis de ambiente para conexão com DB

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -4,7 +4,26 @@ import (
 	"github.com/spf13/viper"
 )
 
-func LoadConfig(_ string) error {
-	viper.AutomaticEnv()  // Carrega automaticamente as variáveis de ambiente
-	return nil
+type Config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	if err := viper.Unmarshal(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
 } 
